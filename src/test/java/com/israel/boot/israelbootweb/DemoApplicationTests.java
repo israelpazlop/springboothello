21eb679d5812fbd1cc951ba11aba11bfac285265
paznogard <paznogard@gmail.com>
2020-08-01 16:14:51 -0500

    first commit

---
+++ src/test/java/com/israel/boot/israelbootweb/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.israel.boot.israelbootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
